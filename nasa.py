bd78ed717081955c165c89b5a5f3a0572d796c89
Amritanshu Agrawal <amritbhanu@users.noreply.github.com>
2016-03-31 01:45:23 -0400

    Update nasa.py

--- nasa.py
+++ nasa.py
@@ -2,13 +2,6 @@ __author__ = 'amrit'
 
 import csv
 
-'''/home/amrit/GITHUB/vcl_logs/pitsA.csv
-/home/amrit/GITHUB/vcl_logs/pitsB.csv
-/home/amrit/GITHUB/vcl_logs/pitsC.csv
-/home/amrit/GITHUB/vcl_logs/pitsD.csv
-/home/amrit/GITHUB/vcl_logs/pitsE.csv
-/home/amrit/GITHUB/vcl_logs/pitsF.csv'''
-
 fo=open('pits','w')
 files=['pitsA.csv','pitsB.csv','pitsC.csv','pitsD.csv','pitsE.csv','pitsF.csv']
 for i in files:
